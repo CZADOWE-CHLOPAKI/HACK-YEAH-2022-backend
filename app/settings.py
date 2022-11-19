79c17390c47fde5b51131dd2fbb2c2c4a0766360
maciek <macieq2048@gmail.com>
2022-11-19 13:55:39 +0100

    move db parameters to settings.py file

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,9 @@
 from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MYSQL_USER = environ.get('MYSQL_USER', 'sendhybrid')
 MYSQL_DB = environ.get('MYSQL_DB', 'sendhybrid')
-MYSQL_HOST = environ.get('MYSQL_DB', 'localhost:3306')
+MYSQL_PASSWORD = environ.get('MYSQL_PASSWORD', 'sendhybrid')
+MYSQL_HOST = environ.get('MYSQL_HOST', 'localhost:3306')
--- app/src/database.py
+++ app/src/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from app import settings
+
 # SQLALCHEMY_DATABASE_URL = "mysql://localhost:3306/test"
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/sendhybrid"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{settings.MYSQL_USER}:{settings.MYSQL_PASSWORD}@{settings.MYSQL_HOST}/{settings.MYSQL_DB}"
 
 engine = create_engine(
         SQLALCHEMY_DATABASE_URL
