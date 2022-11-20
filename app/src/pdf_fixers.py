e27a821d731e3e74d3fa188902b02ade17111cf2
maciek <macieq2048@gmail.com>
2022-11-20 03:45:56 +0100

    fix cleaning up empty pages from pdfs

--- app/src/pdf_fixers.py
+++ app/src/pdf_fixers.py
@@ -8,7 +8,6 @@ from app.src.converters import ConvertedFile
 def remove_empty_pages(file_in: ConvertedFile):
     pdf_writer = PdfFileWriter()
     pdf_reader = PdfFileReader(file_in.file_path)
-
     no_pages = pdf_reader.getNumPages()
 
     page_sizes = []
@@ -18,11 +17,11 @@ def remove_empty_pages(file_in: ConvertedFile):
 
     pages_to_add = [i for i, size in enumerate(page_sizes) if size > 200]
 
-    for page in pages_to_add:
-        pg = pdf_reader.getPage(page)
-        pdf_writer.addPage(pg)
+    if len(page_sizes) != len(pages_to_add):
+        for page in pages_to_add:
+            pdf_writer.addPage(pdf_reader.getPage(page))
 
-    with open(file_in.file_path, 'wb') as fh:
-        pdf_writer.write(fh)
+        with open(file_in.file_path, 'wb') as fh:
+            pdf_writer.write(fh)
 
-    file_in.add_error("Z pliku pdf zostały usunięte puste strony", corrected=True)
+        file_in.add_error("Z pliku pdf zostały usunięte puste strony", corrected=True)
